63bdd991eb43f8f37ed5b8d468fee0132a9dc124
gaiaraw <gaiaberkache@yahoo.fr>
2024-07-18 11:45:34 +0200

    Premier commit

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from flask import render_template
+
+from app import app
+
+@app.route('/')
+@app.route('/index.html')
+def home():
+    return render_template("index.html")
+
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
