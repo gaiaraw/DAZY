c62ee1e0e4f59b18258f672e7df39dd9310bbe69
gaiaraw <gaiaberkache@yahoo.fr>
2024-08-07 19:40:59 +0200

    router link

--- frontend/dazy-frontend/src/components/Dazy.vue
+++ frontend/dazy-frontend/src/components/Dazy.vue
@@ -5,7 +5,7 @@
         </div>
         <div>
             <router-link to="/new">
-                <img src="@/assets/Fiore_01.svg" alt="DAzyzyz" style="width: 50%;">
+                <img id="dazy" src="@/assets/DAZ_LOGO_FLEUR.svg" alt="dazy" title="dazy">
             </router-link>
             
         </div>
@@ -16,3 +16,16 @@
 const props = defineProps(['test']);
 
 </script> 
+
+<style>
+#dazy{
+    width: 50%;
+    transition: filter 0.5s ease;
+}
+#dazy:hover {
+    transform: scale(1.15); /* Agrandit légèrement l'image */
+    filter: brightness(0) saturate(100%) invert(12%) sepia(100%) saturate(1800%) hue-rotate(250deg) brightness(100%) contrast(100%);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Ajoute une ombre */
+}
+
+</style>
